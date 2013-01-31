4e610c55c940f56ca5b94398ae4aa89d6a63ee93
Anirudh <mathukumillia@s.dcsdk12.org>
2013-01-31 09:00:30 -0700

    some progress on recursive

--- palindrome.java
+++ palindrome.java
@@ -1,8 +1,25 @@
+/**
+ * 		palindrome 
+ *   		checks if a given word is a palindrome and returns true or false
+ *   		this class does this in both a recursive and iterative fashion
+ */
+
 public class palindrome{
 	public static void main(String[] args) {
 		System.out.println(iterativePalindrome(args[0]));
 		System.out.println(recursivePalindrome(args[0]));
 	}
+
+	/**
+	 *	iterativePalindrome
+	 *		check if a word is a palindrome iteratively
+	 *	pre: the string being passed in must not be null
+	 * 	post: the method returns a boolean
+	 * 
+	 * 	@param word - the word that needs to be checked if its a palindrome
+	 * 	@return - true or false depending on if the word is a palindrome
+	 * 
+	 */
 	public static boolean iterativePalindrome(String word){
 		for(int i = 0; i < word.length(); i++){
 			if(! (word.charAt(i) == word.charAt((word.length() - 1)-i)) ){
@@ -11,7 +28,14 @@ public class palindrome{
 		}
 		return true;
 	}
+
+
 	public static boolean recursivePalindrome(String word){
-		
+		if(word.length() == 0){
+			return true;
+		}
+		if(! word.charAt(0) == word.charAt(word.length()-1)){
+			return false;
+		}
 	}
 }
